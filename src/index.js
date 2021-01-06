e8f2a370221386a0ed3cdfb4f77e5362305041ea
itsyarl <yarl.van.onckelen@gmail.com>
2021-01-06 21:35:17 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const App = () => {
+  return <p>React Startproject</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
